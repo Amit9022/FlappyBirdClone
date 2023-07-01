3294abf3a40286bbd2cf535e9e23fbe9134dabea
Abhishek Yadav <x0abhie@gmail.com>
2023-07-02 00:44:47 +0530

    first commit

--- App.js
+++ App.js
@@ -1,56 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
-import { GameEngine } from 'react-native-game-engine';
-import entities from './entities';
-import Physics from './physics';
+import { StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { GameEngine } from "react-native-game-engine";
+import entities from "./entities";
+import Physics from "./physics";
 
 export default function App() {
-  const [running, setRunning] = useState(false)
-  const [gameEngine, setGameEngine] = useState(null)
-  const [currentPoints, setCurrentPoints] = useState(0)
+  const [running, setRunning] = useState(false);
+  const [gameEngine, setGameEngine] = useState(null);
+  const [currentPoints, setCurrentPoints] = useState(0);
   useEffect(() => {
-    setRunning(false)
-  }, [])
+    setRunning(false);
+  }, []);
   return (
     <View style={{ flex: 1 }}>
-      <Text style={{ textAlign: 'center', fontSize: 40, fontWeight: 'bold', margin: 20 }}>{currentPoints}</Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          margin: 10,
+          alignItems: "center",
+          alignSelf: "center",
+          zIndex:100
+        }}
+      >
+        <Text style={{ textAlign: "center", fontSize: 30, fontWeight: "bold" }}>
+          Score:
+        </Text>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            fontWeight: "600",
+            marginLeft: 5,
+            marginTop: 2,
+          }}
+        >
+          {currentPoints}
+        </Text>
+      </View>
       <GameEngine
-        ref={(ref) => { setGameEngine(ref) }}
+        ref={(ref) => {
+          setGameEngine(ref);
+        }}
         systems={[Physics]}
         entities={entities()}
         running={running}
         onEvent={(e) => {
           switch (e.type) {
-            case 'game_over':
-              setRunning(false)
-              gameEngine.stop()
+            case "game_over":
+              setRunning(false);
+              gameEngine.stop();
               break;
-            case 'new_point':
-              setCurrentPoints(currentPoints + 1)
+            case "new_point":
+              setCurrentPoints(currentPoints + 10);
               break;
           }
         }}
-        style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }}
+        style={{ position: "absolute", top: 0, left: 0, right: 0, bottom: 0 }}
       >
         <StatusBar style="auto" hidden={true} />
-
       </GameEngine>
 
-      {!running ?
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity style={{ backgroundColor: 'black', paddingHorizontal: 30, paddingVertical: 10 }}
+      {!running ? (
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              backgroundColor: "blue",
+              paddingHorizontal: 30,
+              paddingVertical: 10,
+              borderRadius: 10,
+            }}
             onPress={() => {
-              setCurrentPoints(0)
-              setRunning(true)
-              gameEngine.swap(entities())
-            }}>
-            <Text style={{ fontWeight: 'bold', color: 'white', fontSize: 30 }}>
+              setCurrentPoints(0);
+              setRunning(true);
+              gameEngine.swap(entities());
+            }}
+          >
+            <Text style={{ fontWeight: "bold", color: "white", fontSize: 20 }}>
               START GAME
             </Text>
           </TouchableOpacity>
-
-        </View> : null}
+        </View>
+      ) : null}
     </View>
   );
 }
